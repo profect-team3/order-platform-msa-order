35fa91280c8a4d77d8f65fac10c39469e324d5ec
huniieee <sehoon981014@gmail.com>
2025-08-21 23:03:55 +0900

    feat(order) circuit breaker consumer 추가
    1. consumer 추가
    2. test할 때 eureka 연결 x

---
+++ src/main/java/app/global/config/Resilience4jConfig.java
@@ -0,0 +1,35 @@
+package app.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.github.resilience4j.circuitbreaker.CircuitBreaker;
+import io.github.resilience4j.core.registry.EntryAddedEvent;
+import io.github.resilience4j.core.registry.EntryRemovedEvent;
+import io.github.resilience4j.core.registry.EntryReplacedEvent;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Configuration
+public class Resilience4jConfig {
+
+    @Bean
+    public RegistryEventConsumer<CircuitBreaker> circuitBreakerEventConsumer() {
+        return new RegistryEventConsumer<>() {
+            @Override
+            public void onEntryAddedEvent(EntryAddedEvent<CircuitBreaker> entryAddedEvent) {
+                entryAddedEvent.getAddedEntry().getEventPublisher()
+                    .onStateTransition(event -> log.info("CircuitBreaker state changed: {}", event.getStateTransition()));
+            }
+
+            @Override
+            public void onEntryRemovedEvent(EntryRemovedEvent<CircuitBreaker> entryRemoveEvent) {
+            }
+
+            @Override
+            public void onEntryReplacedEvent(EntryReplacedEvent<CircuitBreaker> entryReplacedEvent) {
+            }
+        };
+    }
+}
--- src/test/java/app/CircuitBreakerTest.java
+++ src/test/java/app/CircuitBreakerTest.java
@@ -2,6 +2,8 @@ package app;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreaker;
 import io.github.resilience4j.circuitbreaker.CircuitBreakerRegistry;
+import io.github.resilience4j.core.registry.RegistryEventConsumer;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +13,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest
 @ActiveProfiles("test")
 public class CircuitBreakerTest {
+
+    @Autowired
+    private RegistryEventConsumer<CircuitBreaker> registryEventConsumer;
 
     @Autowired
     private CircuitBreakerRegistry circuitBreakerRegistry;
